ae59bff211311bab8b2dc86845b87e105505252f
Fazeleh Kazemian <fazeleh.kazemian@gmail.com>
2017-09-20 13:18:58 +1000

    Updated the code to make it more efficient. I had incorrectly passed an array f_value to the function bellman_operator, which I removed in this version.

--- Basic_RBC6.jl
+++ Basic_RBC6.jl
@@ -1,106 +1,141 @@
 workspace()
+#====================================#
+#  K : capitalSteadyState
+#  cs : consumptionSteadyState
+#  z : vProductivity
+#  k_grid : GridCapital
+#  out : outputSteadyState
+#  T : transition matrix
+#====================================#
+
 type BasicRBC{T <: Real}
-    aalpha::Float64
-    bbeta::Float64
-    vProductivity::Matrix{T}
-    mTransition::Matrix{T}
-    capitalSteadyState::Float64
-    outputSteadyState::Float64
-    consumptionSteadyState::Float64
-    vGridCapital::Vector{T}
+    α::Float64
+    β::Float64
+    z::Matrix{T}
+    T::Matrix{T}
+    K::Float64
+    out::Float64
+    cs::Float64
+    k_grid::Vector{T}
 end
 
-function BasicRBC(;aalpha = 1/3,
-                        bbeta = 0.95,
-                        vProductivity = [0.9792 0.9896 1.0000 1.0106 1.0212],
-                        mTransition = [0.9727 0.0273 0.0000 0.0000 0.0000;
-                                       0.0041 0.9806 0.0153 0.0000 0.0000;
-                                       0.0000 0.0082 0.9837 0.0082 0.0000;
-                                       0.0000 0.0000 0.0153 0.9806 0.0041;
-                                       0.0000 0.0000 0.0000 0.0273 0.9727])
+
+function BasicRBC(;α = 1/3,
+                   β = 0.95,
+                   z = [0.9792 0.9896 1.0000 1.0106 1.0212],
+                   T = [0.9727 0.0273 0.0000 0.0000 0.0000;
+                                  0.0041 0.9806 0.0153 0.0000 0.0000;
+                                  0.0000 0.0082 0.9837 0.0082 0.0000;
+                                  0.0000 0.0000 0.0153 0.9806 0.0041;
+                                  0.0000 0.0000 0.0000 0.0273 0.9727])
     
-    capitalSteadyState = (aalpha*bbeta)^(1/(1-aalpha))
-    outputSteadyState = capitalSteadyState^aalpha
-    consumptionSteadyState = outputSteadyState-capitalSteadyState
-    println("Output = ",outputSteadyState," Capital = ",capitalSteadyState," Consumption = ",consumptionSteadyState)
-    vGridCapital = collect(0.5*capitalSteadyState:0.00001:1.5*capitalSteadyState)
-    BasicRBC(aalpha,bbeta,vProductivity,mTransition,capitalSteadyState,outputSteadyState,
-                    consumptionSteadyState,vGridCapital)
+    K = (α*β)^(1/(1-α))
+    out = K^α
+    cs = out-K
+    println("Output = ",out," Capital = ",K," Consumption = ",cs)
+    k_grid = collect(0.5*K:0.00001:1.5*K)
+    BasicRBC(α,β,z,T,K,out,cs,k_grid)
    
 end
     
-l(x)=length(x)
+
+
+_unpack(p::BasicRBC) = (p.α,p.β,p.k_grid,p.z,p.T )
+
+
+#====================================#
+# i : nProductivity
+# j : nCapital
+# l : nCapitalNextPeriod
+# c : consumption
+# v : valueProvisional
+# v_max : valueHighSoFar
+# k_choice : capitalChoice
+# kNext : gridCapitalNextPeriod
+#====================================#
+
+function bellman_operator!(p::BasicRBC,
+                           output::Matrix,
+                           f_valueN::Matrix,
+                           f_policy::Matrix,
+                           E::Matrix)
+    
+    α,β,k_grid,z =_unpack(p)
+    n = length(k_grid)
+    m = length(z)    
+    #-----------------------------------------------------------------
+    for i = 1:m
+    # We start from previous choice (monotonicity of policy function)
+        kNext = 1
+        for j = 1:n
+            v_max = -1000.0
+            k_choice  = k_grid[1]
+                for l = kNext : n
+                    c = output[j,i]-k_grid[l]
+                    v = (1-β)*log(c)+β*E[l,i]
+                    if v > v_max
+                        v_max = v
+                        k_choice = k_grid[l]
+                        kNext = l
+                    else
+                        break # We break when we have achieved the max
+                    end
+                                 
+                end
+                f_valueN[j,i] = v_max
+                f_policy[j,i] = k_choice
+          end
+    end
+    #-----------------------------------------------------------------
+    
+end
+
+
+#====================================#
+# m : length of GridProductivity(z)
+# n : length of GridCapital(k_grid)
+# tol : tolerance
+# E : expectedValueFunction
+# f_value : mValueFunction
+# f_valueN : mValueFunctionNew
+# f_policy : mPolicyFunction
+# error : maxDifference
+#iter : iteration
+#====================================#
 
 function main()
     
-       v=BasicRBC()
-       aalpha,bbeta,vGridCapital,vProductivity,mTransition = v.aalpha,v.bbeta,v.vGridCapital,v.vProductivity,v.mTransition    
-       nGridCapital,nGridProductivity=map(l,[vGridCapital,vProductivity])
-          
-        # Required matrices and vectors
-        mOutput           = zeros(nGridCapital,nGridProductivity)
-        mValueFunction    = zeros(nGridCapital,nGridProductivity)
-        mValueFunctionNew = zeros(nGridCapital,nGridProductivity)
-        mPolicyFunction   = zeros(nGridCapital,nGridProductivity)
-        expectedValueFunction = zeros(nGridCapital,nGridProductivity)
-
-        #We pre-build output for each point in the grid
-        mOutput = (vGridCapital.^aalpha)*vProductivity;
-
-        # Main iteration
-        maxDifference = 10.0
-        tolerance = 0.0000001
-        iteration = 0
-
-        while(maxDifference > tolerance)
-            expectedValueFunction = mValueFunction*mTransition';
-
-            for nProductivity = 1:nGridProductivity
-        
-            # We start from previous choice (monotonicity of policy function)
-                gridCapitalNextPeriod = 1
-        
-                for nCapital = 1:nGridCapital
-        
-                    valueHighSoFar = -1000.0
-                    capitalChoice  = vGridCapital[1]
-            
-                    for nCapitalNextPeriod = gridCapitalNextPeriod:nGridCapital
-
-                        consumption = mOutput[nCapital,nProductivity]-vGridCapital[nCapitalNextPeriod]
-                        valueProvisional = (1-bbeta)*log(consumption)+bbeta*expectedValueFunction[nCapitalNextPeriod,nProductivity]
-               
-                        if (valueProvisional>valueHighSoFar)
-                        valueHighSoFar = valueProvisional
-                        capitalChoice = vGridCapital[nCapitalNextPeriod]
-                        gridCapitalNextPeriod = nCapitalNextPeriod
-                        else
-                        break # We break when we have achieved the max
-                        end
-                                 
-                    end
-            
-                    mValueFunctionNew[nCapital,nProductivity] = valueHighSoFar
-                    mPolicyFunction[nCapital,nProductivity] = capitalChoice
-          
-                end
-        
-            end
-
-            maxDifference  = maximum(abs.(mValueFunctionNew-mValueFunction))
-            mValueFunction    = mValueFunctionNew
-            mValueFunctionNew = zeros(nGridCapital,nGridProductivity)
-
-            iteration = iteration+1
-            if mod(iteration,10)==0 || iteration == 1
-                println(" Iteration = ", iteration, " Sup Diff = ", maxDifference)
-            end
-           
+        p=BasicRBC()
+        α,β,k_grid,z,T = _unpack(p)
+        n = length(k_grid)
+        m = length(z)
+        #-----------------------------------------------------------------
+        ouput    = zeros(n,m)
+        f_value  = zeros(n,m)
+        f_valueN = zeros(n,m)
+        f_policy = zeros(n,m)
+        E = zeros(n,m)
+        output = (k_grid.^α)*z;
+        #-----------------------------------------------------------------
+        error = 10.0
+        tol = 0.0000001
+        iter = 0
+        #-----------------------------------------------------------------
+        while error > tol
+            E = f_value*T';
+            bellman_operator!(p ,Matrix(output),Matrix(f_valueN),Matrix(f_policy),Matrix(E))
+            error  = maximum(abs.(f_valueN - f_value))
+            f_value    = f_valueN
+            f_valueN = zeros(n,m)
+            iter = iter + 1
+            if mod(iter,10)==0 || iter == 1
+                println(" Iteration = ", iter, " Sup Diff = ", error)
+            end 
         end
-
-        println(" Iteration = ", iteration, " Sup Diff = ", maxDifference)
+        #-----------------------------------------------------------------
+        println(" Iteration = ", iter, " Sup Diff = ", error)
         println(" ")
-        println(" My check = ", mPolicyFunction[1000,3])
+        println(" My check = ", f_policy[1000,3])
 
-    end
+end
 
